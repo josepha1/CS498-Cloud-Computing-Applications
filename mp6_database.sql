5061f1f07cd77b682c46371fd1e3d630f874a6a0
Joseph Allen <josepha1@email.arizona.edu>
2022-03-26 20:25:18 -0700

    Add files via upload

---
+++ mp6_database.sql
@@ -0,0 +1,10 @@
+CREATE database mp6_db;
+CREATE TABLE mp6_table (
+    id INT UNSIGNED AUTO_INCREMENT,
+    hero VARCHAR(30),
+    power VARCHAR(30),
+    name VARCHAR(30),
+    xp INT(30),
+    color VARCHAR(30),
+    PRIMARY KEY(id);
+)
